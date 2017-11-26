0bf62ccca75a78911994b27ded7e8d63a2a464c6
typhooneye <32542969+typhooneye@users.noreply.github.com>
2017-11-27 01:11:42 +0800

    jixuan3.2 所有公式适用的误差传递函数
    好好按照格式输入哦

--- err.m
+++ err.m
@@ -1,4 +1,4 @@
-function errors = err(func,argument,org_errs,org_val)
+nction errors = err(func,argument,org_errs,org_val)
 %errors = err(func,argument,org_errs),返回一个运用误差传递公式计算得出的误差值。
 %输入变量说明：
 %func 字符型变量 函数表达式，等式右边部分
@@ -13,10 +13,10 @@ function errors = err(func,argument,org_errs,org_val)
 num = size(argument);%得到带误差变量数量
 FUNC = sym(func);
 errors2 = 0;
-for n =1:num(2);
+for n =1:num(2);%把所有变量变成符号
     sym(argument(n))
 end
-for n = 1:num(2);
+for n = 1:num(2);%对每一个变量求偏导，带值计算并乘以误差值
     sub_func = sym(diff(FUNC,sym(argument(n))));%偏导
     errors2 = errors2+(subs(sub_func,{argument(:)},{org_val(:)})*org_errs(n))^2;
 end
