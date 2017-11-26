81bb6faf86aee1c28bbcb09a92cc9b0196afe256
typhooneye <32542969+typhooneye@users.noreply.github.com>
2017-11-26 23:19:05 +0800

    Update gra.m

--- gra.m
+++ gra.m
@@ -1,10 +1,10 @@
 function F=gra(m1,m2,r)
-%F=gra(m1,m2,r) �������������ļ���ֵ
-%�������˵����
-%m1 ��ֵ�ͱ���
-%m2 ��ֵ�ͱ���
-%r ��ֵ�ͱ���
-%ע��ͳһ��λ
+%F=gra(m1,m2,r) 返回万有引力的计算值
+%输入变量说明：
+%m1 数值型变量 天体1质量
+%m2 数值型变量 天体2质量
+%r 数值型变量 两天体距离
+%注意统一单位
 format shortE
 syms M1 M2 R G
 GRA = sym('G*M1*M2/(R^2)');
