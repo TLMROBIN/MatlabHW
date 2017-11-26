3b22b324025f08eabcb07442f3e95c0df669d45c
FontainebleauYuyuko <33483342+FontainebleauYuyuko@users.noreply.github.com>
2017-11-26 14:36:57 +0800

    主程序「调用万有引力公式及其误差分析公式」

---
+++ Homework 1/wyylwcfx.m
@@ -0,0 +1,8 @@
+m1=input('«Î ‰»Îm1:');
+m2=input('«Î ‰»Îm2:');
+R=input('«Î ‰»ÎR:');
+deltam1=input('«Î ‰»Îdeltam1:');
+deltam2=input('«Î ‰»Îdeltam2:');
+deltaR=input('«Î ‰»ÎdeltaR:');
+display(wyyl(m1,m2,R))
+display(wyylwc(m1,m2,R,deltam1,deltam2,deltaR))
