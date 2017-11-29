42e97d0fce59650b481fba1236dcfcce0f44f1c5
Henry <33482075+mahengrui1@users.noreply.github.com>
2017-11-29 20:04:52 +0800

    Less the sym

--- homework 12nd week/gra_dev_f.m
+++ homework 12nd week/gra_dev_f.m
@@ -1,9 +1,8 @@
-function F_dev=gra_dev_f(m1_p,m2_p,r_p,m1_dev_p,m2_dev_p,r_dev_p)
+function F_dev=gra_dev_f(m1,m2,r,m1_dev,m2_dev,r_dev)
 %To input m1, m2 and r and their dev in turn then it outputs F's dev.
 %
 %Henry, BNU, 20171122
-m1=sym(m1_p);m2=sym(m2_p);r=sym(r_p);m1_dev=sym(m1_dev_p);m2_dev= ...
-    sym(m2_dev_p);r_dev=sym(r_dev_p);G=sym(6.67e-11);
+G=6.67e-11;
 F_=sym('G*m1*m2/r^2');
 F_dev2=(diff(F_,'m1')*sym('m1_dev'))^2+(diff(F_,'m2')*sym('m2_dev'))^2+( ...
     diff(F_,'r')*sym('r_dev'))^2; %To create dev's square value formula.
--- homework 12nd week/gra_f.m
+++ homework 12nd week/gra_f.m
@@ -2,9 +2,8 @@ function F=gra_f(m1,m2,r)
 %To input m1, m2 and r then it outputs F.
 %
 %Henry, BNU, 20171122
-%m1=sym(m1_p);m2=sym(m2_p);r=sym(r_p);
 G=6.67e-11;
-F_=sym('G*m1*m2/r^2');
-eval(['gra=sym(',char(F_),');']); %it is better than 'subs'
+F_='G*m1*m2/r^2';
+eval(['gra=sym(',F_,');']); 
 F=double(gra);
 end
