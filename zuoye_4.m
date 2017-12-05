9fdade9843996b721b360fa2d8380407f4406a98
ussbogue <33482969+ussbogue@users.noreply.github.com>
2017-12-05 09:27:06 -0600

    Add files via upload

--- zuoye_4.m
+++ zuoye_4.m
@@ -1,26 +1,26 @@
 clear,clc
-fid = fopen('exp_data.txt','r')
-time=zeros(1,3);
-day=zeros(1,3);
-date1=zeros(4,4);
-date2=zeros(4,4);
-date3=zeros(4,4);
-date4=zeros(4,4);
+fid = fopen('exp_data.txt','r');
+time=cell(1,3);
+day=cell(1,3);
+date=cell(4,4);
 tline = fgets(fid);                                                                                                                                                                                                                         
 tline = fgets(fid);
 tline = fgets(fid);
 for i=1:3;
     tline = fgets(fid);
-    time(i)=tlinew
+    time{i}=tline;
     tline = fgets(fid);
-    day(i)=tline;
+    day{i}=tline;
     tline = fgets(fid);
-    date1(:,i)=tline;
+    date{1,i}=tline;
     tline = fgets(fid);
-    date2(:,i)=tline;
+    date{2,i}=tline;
     tline = fgets(fid);
-    date3(:,i)=tline;
+    date{3,i}=tline;
     tline = fgets(fid);
-    date4(:,i)=tline;
+    date{4,i}=tline;
 end
 fclose(fid); 
+time
+day
+date
