081c2ceb47e1e5505db1c8225fdd59b0d4716628
nianyi2333 <33688177+nianyi2333@users.noreply.github.com>
2017-12-06 17:53:24 +0800

    Add files via upload
    老师，发现之前的关于误差函数的有错误，我又重新做了一个关于errorUG的.m文件

--- errorUG.m
+++ errorUG.m
@@ -1,8 +1,9 @@
-function errorF=errorUG(G,m1,m2,R,errorG,errorm1,errorm2,errorR)
-F=G.*m1.*m2/(R.^2);
-partialderivativeG=diff(F,G,1);
+function errorF=errorUG(M1,M2,RR,ErrorM1,ErrorM2,ErrorR)
+syms m1 m2 R errorm1 errorm2 errorR
+F=6.67*10^-11*m1.*m2/(R.^2);
 partialderivativem1=diff(F,m1,1);
 partialderivativem2=diff(F,m2,1);
 partialderivativeR=diff(F,R,1);
-errorFsquare=(partialderivativeG.*errorG).^2+(partialderivativem1.*errorm1).^2+(partialderivativem2.*errorm2).^2+(partialderivativeR.*errorR).^2
+errorFsquare=(partialderivativem1.*errorm1).^2+(partialderivativem2.*errorm2).^2+(partialderivativeR.*errorR).^2
+subs(errorFsquare,[m1,m2,R,errorm1,errorm2,errorR],[M1,M2,RR,ErrorM1,ErrorM2,ErrorR])
 errorF=sqrt(errorFsquare)
