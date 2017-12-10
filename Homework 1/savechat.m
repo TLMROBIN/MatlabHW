6b2a3cc9c7734ab6d9114d2f3b34624b2717cf22
wrq0306 <33482481+wrq0306@users.noreply.github.com>
2017-12-10 23:56:35 +0800

    2017.11.29     Homework4
    读取并分别保存有用信息
    1、打开数据读取
    2、录入标题、数据数并输出
    3、空行
    4、按行从下读取，分别把数据存入对应变量，注意数组的转置
    5、将数据分类存在三个矩阵并输出
    6、保存有用信息至文件

--- Homework 1/savechat.m
+++ Homework 1/savechat.m
@@ -1,22 +1,22 @@
-fid=fopen('exp_data.txt','r');
+fid=fopen('exp_data.txt','r');  %打开文件读取
 title=fgetl(fid)
 number=fgetl(fid)
 fgetl(fid);
 
-time1=fgetl(fid);
+time1=fgetl(fid);                %存第一组数组
 date1=fgetl(fid);
 datas1=(fscanf(fid,'%f',[4,4]))';
-fgetl(fid);
-time2=fgetl(fid);
+fgetl(fid);       
+time2=fgetl(fid);                 %存第二组数据
 date2=fgetl(fid);
 datas2=(fscanf(fid,'%f',[4,4]))';
 fgetl(fid);
-time3=fgetl(fid);
+time3=fgetl(fid);                 %存第三组数据
 date3=fgetl(fid);
 datas3=(fscanf(fid,'%f',[4,4]))';
 
-time=char(time1,time2,time3)
+time=char(time1,time2,time3)  %输出数据并将三组数据分类整合
 date=char(date1,date2,date3)
 datas=[datas1;datas2;datas3]
 
-save('observedata.mat','title','number','time','date','datas')
+save('observedata.mat','title','number','time','date','datas')  %保存
