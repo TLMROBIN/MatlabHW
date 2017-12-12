7b9499e5d7c9d0d11e0b8e5e45f72abb0c1e63a9
wangtianc <33483045+wangtianc@users.noreply.github.com>
2017-12-12 17:23:18 +0800

    第五次作业
    导入数据并在筛选后作图

---
+++ zuoye5.m
@@ -0,0 +1,39 @@
+clear;close;clc
+newdata=zeros(1,10);
+%import data
+lamost = importdata('lamost.csv',',',1);
+%choose data
+for i=1:43455;                          
+    if lamost.data(i,5)>4.0;
+        if 4000<lamost.data(i,3)<8000;
+            newdata=[newdata;lamost.data(i,:)];
+        end
+    end
+end
+newdata(1,:)=[];
+ra=newdata(:,1);
+dec=newdata(:,2);
+teff=newdata(:,3);
+logg=newdata(:,5);
+feh=newdata(:,7);
+rverr=newdata(:,10);
+%make picture
+figure; 
+%ra_decÉ¢µãÍ¼
+subplot(2,2,1);
+plot(ra,dec,'b.');
+title('ra-decÉ¢µãÍ¼');
+xlabel('ra');
+ylabel('dec');
+%teff-loggÉ¢µãÍ¼
+subplot(2,2,2);
+scatter(teff,logg,[1],feh);
+title('teff-loggÉ¢µãÍ¼');
+xlabel('teff');
+ylabel('logg');
+%rverrÖù×´Í¼
+subplot(2,2,3)
+hist(rverr(rverr>0 & rverr<50),50);
+title('rverrÖù×´Í¼');
+xlabel('rverr');
+ylabel('number');
