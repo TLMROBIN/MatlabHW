7d5e765c6d6517ac4992250db8b614d5a485c848
Henry <33482075+mahengrui1@users.noreply.github.com>
2017-12-13 21:33:05 +0800

    Correct a fault mistake.

--- homework 15th week/myintegral.m
+++ homework 15th week/myintegral.m
@@ -3,8 +3,10 @@ function S=myintegral(x,y,a,b)
 %a as points lower limit and b as points limit, then it outputs integral.
 %Example
 %x = 0:pi/16:pi;y = sin(x);myintegral(x,y,0,pi) →ans=2
+%Warning: must exist a<=x<=b.
 %Henry, BNU, 201611160111, 20171213
-xp=linspace(a,b,length(x)*100);%The 100 bases on my experience. 
-yp=interp1(x,y,xp,'spline');%The spline bases on my experience. 
+y=y(x>=a & x<=b);x=x(x>=a & x<=b);
+xp=linspace(a,b,length(x)*100);%The 100 bases on my experience.
+yp=interp1(x,y,xp,'spline');%The spline bases on my experience.
 S=sum((yp(1:end-1)+yp(2:end)).*(xp(2)-xp(1))./2);
 end
