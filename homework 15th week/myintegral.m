fa5c40bd87393e521fe583f380261705604af3fd
Henry <33482075+mahengrui1@users.noreply.github.com>
2017-12-13 20:58:49 +0800

    Take your interest as your career.

---
+++ homework 15th week/myintegral.m
@@ -0,0 +1,10 @@
+function S=myintegral(x,y,a,b)
+%To input x as matrix of argument and y as matrix of dependent variable and
+%a as points lower limit and b as points limit, then it outputs integral.
+%Example
+%x = 0:pi/16:pi;y = sin(x);myintegral(x,y,0,pi) →ans=2
+%Henry, BNU, 201611160111, 20171213
+xp=linspace(a,b,length(x)*100);%The 100 bases on my experience. 
+yp=interp1(x,y,xp,'spline');%The spline bases on my experience. 
+S=sum((yp(1:end-1)+yp(2:end)).*(xp(2)-xp(1))./2);
+end
